6a3120638ee159cddb89cb2d97efd37e90f51851
Nam Nguyen <merryblue.deva2@gmail.com>
2022-06-10 10:14:21 +0700

    Fix onboarding & policy screen

--- app/src/main/java/com/vuthaihung/loseflat/ui/activities/PolicyActivity.java
+++ app/src/main/java/com/vuthaihung/loseflat/ui/activities/PolicyActivity.java
@@ -1,9 +1,11 @@
 package com.vuthaihung.loseflat.ui.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.vuthaihung.loseflat.R;
 import com.vuthaihung.loseflat.ui.base.BaseActivity;
+import com.vuthaihung.loseflat.ui.customViews.CustomScrollView;
 
 public class PolicyActivity extends BaseActivity {
     @Override
@@ -11,11 +13,20 @@ public class PolicyActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_policy);
         initViews();
+        initEvents();
     }
 
-    private void initViews() {
+    private void initEvents() {
         findViewById(R.id.iv_back_policy).setOnClickListener(view -> {
             onBackPressed();
         });
+        findViewById(R.id.tv_enable_scroll).setOnClickListener(view -> {
+            findViewById(R.id.ln_enable_scroll).setVisibility(View.GONE);
+            ((CustomScrollView)findViewById(R.id.sv_policy)).setScrollingEnabled(true);
+        });
+    }
+
+    private void initViews() {
+        ((CustomScrollView)findViewById(R.id.sv_policy)).setScrollingEnabled(false);
     }
 }
